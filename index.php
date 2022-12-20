8c9398c884c6deada4aa78329ce29bb5ba8532e1
Imtiaz Epu <imtiazepu@programmer.net>
2022-12-20 23:34:07 +0600

    connect mysql database

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 require 'functions.php';
 require 'router.php';
+
+//Connect MySQL database.
+
+$dsn = "mysql:host=localhost;port=3306;dbname=db_laraphp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn,);
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
