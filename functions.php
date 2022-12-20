f183241e6699f926ba5fc2517e9000b1d8ce36ba
Imtiaz Epu <imtiazepu@programmer.net>
2022-12-20 18:50:22 +0600

    intial commit

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @param $value
+ * @return void
+ */
+function dd($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+    die();
+}
+
+/**
+ * @param $value
+ * @return bool
+ */
+function urlIs($value): bool
+{
+    return $_SERVER['REQUEST_URI'] === $value;
+}
